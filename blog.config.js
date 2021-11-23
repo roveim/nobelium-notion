8d8a6c319dc8804dea90e9bd16a82366c86219ce
rove <greymz@foxmail.com>
2021-11-23 15:45:19 +0800

    delete archives

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
   postsPerPage: 7,
   sortByDate: false,
   showAbout: true,
-  showArchive: true,
+  showArchive: false,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: '',
